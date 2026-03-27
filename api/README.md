fd29e450900f7696fb1b8abdeee6d030f94d1547
Jose Valerio <contact@josevalerio.com>
2026-03-26 23:14:05 -0400

    saving

--- api/README.md
+++ api/README.md
@@ -9,5 +9,6 @@ For the required environment variables, please see [.env.example](.env.example)
 `GET /` Root. Maps to health check endpoint
 `GET /api/v1/health` Healthcheck endpoint
 `POST /api/v1/payment-intents` Creates a payment intent in stripe
+`POST /api/v1/horoscopes` Creates a horoscope
 
 Catch all --> 404
--- api/internal/server/root.go
+++ api/internal/server/root.go
@@ -6,9 +6,10 @@ import (
 )
 
 func (s *Server) Root(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{
 		"message": "Hi! You probably meant to go to one of the other routes. Make sure to check the documentation!",
 	})
+
 }
