6a8fe0388ab76c91c43b5ef11623aaf7f729a219
Jose Valerio <contact@josevalerio.com>
2026-03-29 00:37:41 -0400

    saving

--- api/internal/middleware/json_header.go
+++ api/internal/middleware/json_header.go
@@ -2,14 +2,11 @@ package middleware
 
 import (
 	"net/http"
-	"slices"
 )
-
-var allowedMethods = []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete}
 
 func JSONHeader(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if slices.Contains(allowedMethods, r.Method) {
+		if r.Method != http.MethodOptions {
 			w.Header().Add("Content-Type", "application/json")
 		}
 		next.ServeHTTP(w, r)
