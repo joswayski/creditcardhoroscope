a366633a4861deff86271f9a1c0738c83631a345
Jose Valerio <contact@josevalerio.com>
2026-03-29 00:35:40 -0400

    saving

--- api/internal/server/root.go
+++ api/internal/server/root.go
@@ -8,7 +8,8 @@ import (
 func (s *Server) Root(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{
-		"message": "Hi! You probably meant to go to one of the other routes. Make sure to check the documentation!",
+		"message":  "Hi! You probably meant to go to one of the other routes. Make sure to check the documentation!",
+		"docs_url": "https://github.com/joswayski/creditcardhoroscope/blob/main/api/internal/server/server.go#L20",
 	})
 
 }
