9b87dc6adc6a3fbb10e8c3eaa0932bae2df80691
Jose Valerio <contact@josevalerio.com>
2026-03-28 22:32:33 -0400

    saving

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,4 +1,4 @@
-import { isRouteErrorResponse, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -7,6 +7,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="bg-fuschia-50">
       <head>
+        <Links />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
