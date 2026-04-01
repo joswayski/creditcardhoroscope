fb435449423dc36f68fd3af6ab61cb11c873cc46
Jose Valerio <contact@josevalerio.com>
2026-03-31 21:00:10 -0400

    saving

--- api/internal/server/horoscopes.go
+++ api/internal/server/horoscopes.go
@@ -1,8 +1,28 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type CreateHoroscopeRequest struct {
+	PaymentIntentId string `json:payment_intent_id`
+}
 
 func (s *Server) CreateHoroscope(w http.ResponseWriter, r *http.Request) {
+	// Check we received something
+	var req CreateHoroscopeRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil || !strings.HasPrefix(req.PaymentIntentId, "pi_") {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": fmt.Sprintf("Invalid payment_intent_id in the request. Received: '%s'", req.PaymentIntentId),
+		})
+		return
+	}
 
-	w.WriteHeader(http.StatusCreated)
+	// Check stripe to make sure it's good
+
 }
