1907abddf0225d7fe0ee3fa9c8a75f4ea35a95ba
Jose Valerio <contact@josevalerio.com>
2026-04-12 21:21:01 -0400

    saving

--- api/internal/server/rate_horoscope.go
+++ api/internal/server/rate_horoscope.go
@@ -1,12 +1,69 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"slices"
+	"strings"
+	"time"
+)
+
+type RateHoroscopeBody struct {
+	PaymentIntentId string `json:"payment_intent_id"`
+	Rating          string `json:"rating"`
+}
+
+var validRatings = []string{"positive", "negative", "neutral"}
 
 func (s *Server) RateHoroscope(w http.ResponseWriter, r *http.Request) {
-	// TODO Get horoscope by external id and rating = null
-	// check if it exists
+	var reqBody RateHoroscopeBody
+	err := json.NewDecoder(r.Body).Decode(reqBody)
+	if err != nil {
+		slog.Error("Error when parsing the request body", "error", err)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "Bad request format",
+		})
+		return
+	}
 
-	// rating is one of the valid statuses
-	// update rating
+	externalId := r.PathValue("id")
+
+	if !slices.Contains(validRatings, reqBody.Rating) || !strings.HasPrefix(reqBody.PaymentIntentId, "pi_") || externalId == "" {
+		slog.Error("Error when parsing the request body", "error", err, "body", reqBody)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "Bad request, invalid rating, horoscope, or payment intent",
+		})
+		return
+	}
+
+	dbResult, err := s.DB.Exec(r.Context(), `
+	UPDATE generations
+	SET rating = $1, updated_at = $2
+	WHERE
+	rating = NULL AND
+	external_id = $3 AND
+	payment_intent_id = (SELECT id from payment_intents WHERE payment_intend_id = $4)`,
+		reqBody.Rating,
+		time.Now().UTC(),
+		externalId,
+		reqBody.PaymentIntentId,
+	)
+
+	if dbResult.RowsAffected() == 0 {
+		slog.Error("Horoscope not found", "body", reqBody)
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "Horoscope not found",
+		})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "Thanks for your feedback!",
+	})
 
 }
